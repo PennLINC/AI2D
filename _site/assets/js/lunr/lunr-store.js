a0202076dbb0fe2823d3d9a764dc9b2b355994c0
Tien Tong <35613222+tien-tong@users.noreply.github.com>
2026-05-28 22:02:57 -0400

    add bhrc

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -59,6 +59,12 @@ var store = [{
         "url": "/AI2D/docs/community/get_support/",
         "teaser": null
       },{
+        "title": "Brazil High Risk Cohort",
+        "excerpt":"About the dataset This is the neuroimaging dataset for the Brazil High Risk Cohort (BHRC). This version of the dataset was curated at the University of Pennsylvania by the Lifespan Informatics and Neuroimaging Center (PennLINC) as part of the Reproducible Brain Chart (RBC) Initiative. PI Giovanni Abrahão Salum Jr. Pedro...","categories": ["docs","datasets"],
+        "tags": ["development"],
+        "url": "/AI2D/docs/datasets/BHRC/",
+        "teaser": null
+      },{
         "title": "Healthy Brain Network",
         "excerpt":"About the dataset This is the neuroimaging dataset for the Healthy Brain Network (HBN). HBN is a study of childhood development collected by the Child Mind Instute located in New York, NY. HBN was collected at four New York Metropolitan Area sites. This version of the dataset was curated at...","categories": ["docs","datasets"],
         "tags": ["development"],
